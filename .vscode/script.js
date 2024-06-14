9b52051a9d30116e71793829e0ba191f712ceeb5
Lucas Bou Anni <lucas.bou.anni@gmail.com>
2024-06-14 17:37:50 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,18 @@
+function toggleMode () {
+const html = document.documentElement
+
+ html.classList.toggle("light")
+
+ const img = document.querySelector("#profile img")
+//se tiver light mode vai ficar uma imagem
+
+if(html.classList.contains('light')) {
+
+
+ } else { 
+ //se tiver sem light mode vai ficar uma imagem
+
+ }
+
+}
+
